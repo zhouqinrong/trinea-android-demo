195820b8d1d9d0f8590b05f737775bd4e7085e80
trinea1989@gmail.com <trinea1989@gmail.com@2354d424-4cad-bd3a-003c-97edf48c60c8>
2013-05-09 13:08:17 +0000

    所有页面添加底部个人主页和相关内容链接 添加下载管理示例 删除无用示例 页面样式调整

---
+++ trunk/src/cn/trinea/android/demo/DemoList.java
@@ -0,0 +1,87 @@
+package cn.trinea.android.demo;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import cn.trinea.android.demo.utils.AppUtils;
+
+/**
+ * demo list 列表
+ * 
+ * @author Trinea 2012-6-17 下午06:14:57
+ */
+public class DemoList extends Activity {
+
+    public static final String    TAG      = "DemoList";
+
+    private static final String[] mStrings = { "SearchView Demo",
+            "ViewPager Multi Fragment Demo\r\n(ViewPager一屏多Fragment)", "DownloadManager Demo",
+            "Gallery Demo", "ViewPager Demo", "Google Map Demo", "Service Demo",
+            "BroadcastReceiver Demo", "发送短信 Demo" };
+
+    private static final int      total    = mStrings.length - 1;
+
+    private Button                trineaInfoTv;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        AppUtils.initTrineaInfo(this, trineaInfoTv, getClass());
+        LinkedList<String> mListItems = new LinkedList<String>();
+        mListItems.addAll(Arrays.asList(mStrings));
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                                                                this,
+                                                                android.R.layout.simple_list_item_1,
+                                                                mListItems);
+
+        ListView demoListView = (ListView)findViewById(R.id.simpleListView);
+        demoListView.setAdapter(adapter);
+        demoListView.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == total - 8) {
+                    Intent intent = new Intent(DemoList.this, SearchViewDemo.class);
+                    startActivity(intent);
+                } else if (position == total - 7) {
+                    Intent intent = new Intent(DemoList.this, ViewPagerMulTiFragmentDemo.class);
+                    startActivity(intent);
+                } else if (position == total - 6) {
+                    Intent intent = new Intent(DemoList.this, DownloadManagerDemo.class);
+                    startActivity(intent);
+                } else if (position == total - 5) {
+                    Intent intent = new Intent(DemoList.this, GalleryDemo.class);
+                    startActivity(intent);
+                } else if (position == total - 4) {
+                    Intent intent = new Intent(DemoList.this, ViewPagerDemo.class);
+                    startActivity(intent);
+                } else if (position == total - 3) {
+                    Intent intent = new Intent(DemoList.this, GoogleMapDemo.class);
+                    startActivity(intent);
+                } else if (position == total - 2) {
+                    Intent intent = new Intent(DemoList.this, ServiceDemo.class);
+                    startActivity(intent);
+                } else if (position == total - 1) {
+                    Intent intent = new Intent(DemoList.this, BroadcastReceiverDemo.class);
+                    startActivity(intent);
+                } else if (position == total) {
+                    Intent intent = new Intent(DemoList.this, SmsManagerDemo.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
+        // startActivity(new Intent(this, DownloadManagerDemo.class));
+    }
+}
